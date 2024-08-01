4d385da71c877441d46d9548db23c080fc92f045
Jayant <jayantparakh2003@gmail.com>
2024-08-01 15:12:41 +0530

    Updates

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoURI = process.env.MONGODB_URI;
+
 app.use(bodyParser.json());
 app.use(cors());
 
-
+// Uncomment if you need logging middleware
 // app.use((req, res, next) => {
 //   console.log(`${req.method} ${req.url}`);
 //   console.log('Headers:', req.headers);
@@ -18,23 +19,23 @@ app.use(cors());
 //   next();
 // });
 
-const postsRouter = require('./routes/posts');
-app.use('/posts', postsRouter);
-app.use('/auth', require('./routes/auth'));
+// Uncomment if you need routes
+// const postsRouter = require('./routes/posts');
+// app.use('/posts', postsRouter);
+// app.use('/auth', require('./routes/auth'));
 
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+mongoose.connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(`Failed to connect to MongoDB ${mongoURI}`, err));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
 app.listen(PORT, () => {
